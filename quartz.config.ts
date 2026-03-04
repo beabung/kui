6a41add08a3f4fc05bd2830dcf9a25cbce206d92
dihwang <dihwang@wezon.com>
2026-03-04 14:58:50 +0900

    local

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,8 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    /* baseUrl: "quartz.jzhao.xyz", */
-	 baseUrl: "beabung.github.io/quartz-test",
+    baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
